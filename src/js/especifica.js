49ed00770852531544437f9a9329e6a501977bca
diegobejar1011 <126354772+diegobejar1011@users.noreply.github.com>
2023-09-10 21:49:02 -0600

    Update especifica.js

--- src/js/especifica.js
+++ src/js/especifica.js
@@ -13,69 +13,31 @@ document.addEventListener("DOMContentLoaded", function() {
   const banner= document.getElementById("banner");
   const poster = document.getElementById("poster");
   const trailer = document.getElementById("link");
-
+  const iconpopular = document.getElementById("iconpopular");
+  const popular= document.getElementById("popular");
 
   fetch(api_url)
     .then((res) => res.json())
     .then((data) => {
+      
       console.log(data);
   
       title.innerHTML = data[0].Nombre;
-
       description.innerHTML= data[0].Descripcion;
-
       lanzamiento.innerHTML= `Estreno: ${data[0].Lanzamiento}`;
-
       banner.style.backgroundImage= `url('/src/images/${data[0].Nombre}-large.jpg')`;
-
       poster.src = `../images/${data[0].Nombre}.jpg`;
-      
       trailer.href = `${data[0].Trailer}`;
 
       const calificacion= data[0].Calificacion;
-
       const porcentaje = (calificacion * 10).toFixed(0) + "%";
 
-      const popular= document.getElementById("popular");
-
       popular.innerHTML= `Popularidad: ${porcentaje}`;
-
-      const iconpopular = document.getElementById("iconpopular");
-
-      if (calificacion > 5) {
-        iconpopular.src = "../images/thumbs-up-solid.svg"; // Usamos doble barra diagonal inversa para rutas en Windows
-      } else {
-        iconpopular.src = "../images/thumbs-down-solid.svg"; // Usamos doble barra diagonal inversa para rutas en Windows
-      }
+      
+      iconpopular.src= calificacion > 5 ? "src\\img\\thumbs-up-solid.svg" : "src\\img\\thumbs-down-solid.svg";
+     );
 
     })
     .catch((err) => console.error(err));
 
 });
-
-
-
-
-function cambiarEspecifica(id){    
-
-  /*
-  const botones = document.querySelectorAll('button[value]')
-
-  console.log(botones)
-
-  botones.forEach(boton => {
-
-    boton.addEventListener('click', cambiarEspecifica)
-    
-  });
-  console.log(botones)
-  */
-
-
-}
-
-
-
-
-
-
